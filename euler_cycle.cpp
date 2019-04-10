d9bca463298b41225fe439c48e015f31d071aae0
gafeol <gabfeol@gmail.com>
2019-04-10 19:40:59 -0300

    Adiciona inicio de CPP, Finaliza prova do teorema de euler, Melhorias na prova do lema

--- euler_cycle.cpp
+++ euler_cycle.cpp
@@ -1,26 +1,27 @@
-// adjacency list
-vector<pii> adj[MAXN];
-
-int mrk[MAXE];
-
-int temp_path[MAXE], final_path[MAXE], tsz, fsz;
-
+bool mrk[E];
+queue<int> final_path;
+stack<int> temp_path;
 int edge_id = 0;
-void add_edge(int a, int b){
-	// undirected edge
-	adj[a].pb(pii(b, edge_id));	
-	adj[b].pb(pii(a, edge_id));
+
+struct edge{
+	int to, id;
+};
+
+vector<edge> adj[N];
+
+void add_edge(int u, int v){
+	adj[a].push_back({b, edge_id});	
+	adj[b].push_back({a, edge_id});
 	edge_id = edge_id + 1;
 }
 
 void euler_dfs(int u){
-	temp_path[tsz++] = u;
-	for(pii ar: adj[u]){
-		int id = ar.snd;
-		if(mrk[id]) continue;
-		mrk[id] = 1;
-		int v = ar.fst; euler_dfs(v);
+	temp_path.push(u);
+	for(edge e: adj[u]){
+		if(mrk[e.id]) continue;
+		mrk[e.id] = true;
+		euler_dfs(e.to);
 	}
-	final_path[fsz++] = temp_path[--tsz];
+	final_path.push(temp_path.top());
+	temp_path.pop();
 }
-// Cycle stored in final_path[0...fsz-1]
