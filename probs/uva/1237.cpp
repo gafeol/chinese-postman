570fb5f695d28e6efd45790d5b7560c4f22fc155
Gabriel <gabfeol@gmail.com>
2019-06-11 21:58:16 -0300

    add code + fix compile error

--- probs/uva/1237.cpp
+++ probs/uva/1237.cpp
@@ -19,14 +19,53 @@ template<typename T> inline T abs(T t) { return t < 0? -t : t; }
 const ll modn = 1000000007;
 inline ll mod(ll x) { return x % modn; }
 
-const int MAXN = 212345;
+const int MAXN = 212;
 
 const ll INF = LLONG_MAX/3ll;
 
 int n, m, k;
 int s[MAXN];
 
-void floyd_warshall(vector<vector<ll>> &mnDist){
+
+const int BITMASK = (1<<16);
+ll dp[BITMASK];
+vector<int> oddVertex; 
+vector<vector<ll>> mnDist;
+
+ll go(int bm){
+    if(bm == 0) return 0;
+    if(dp[bm] != -1) 
+        return dp[bm];
+    dp[bm] = LLONG_MAX;
+    for(int i=0;i<oddVertex.size();i++){
+        if(!(bm&(1<<i))) continue;
+        for(int j=i+1;j<oddVertex.size();j++){
+            if(!(bm&(1<<j))) continue;
+            dp[bm] = min(dp[bm], mnDist[oddVertex[i]][oddVertex[j]] + go(bm ^ (1<<i) ^ (1<<j)));
+        }
+    }
+    return dp[bm];
+}
+
+ll minCostPerfectMatch(int n) {
+    for(int a=0;a<(1<<n);a++){
+        dp[a] = -1;
+    }
+    return go((1<<n)-1);
+}
+
+ll cost_of_duplication(vector<vector<pair<int, ll>>> &adj){
+    oddVertex.clear();
+    for(int a=0;a<n;a++)
+        if(((int)adj[a].size())&1)
+            oddVertex.pb(a);
+
+
+    ll cost = minCostPerfectMatch(oddVertex.size());
+    return cost;
+}
+
+void floyd_warshall(){
     for(int k=0;k<n;k++){
         for(int i=0;i<n;i++){
             for(int j =0;j<n;j++){
@@ -37,34 +76,22 @@ void floyd_warshall(vector<vector<ll>> &mnDist){
 }
 
 
-void prepare_flow(vector<vector<pair<int, ll>>> &adj, vector<vector<ll> &mnDist){
-    for(int a=0;a<n;a++){
-
-    }
-}
-
-void add_edge(int u, int v, ll cost){
-    /// creates an undirected edge between u and v with defined cost
-
-
-}
-
-vector<pair<int, int>> perfect_min_cost_match(vector<pair<int, ll>> &adj, vector<vector<ll>> &mnDist){
-    vector<int> oddVertex;
-    for(int a=0;a<n;a++)
-        if(adj[a].size()&1)
-            oddVertex.pb(a);
-    for(int i=0;i<oddVertex;i++)
-        for(int j=i+1;j<oddVertex;j++)
-            add_edge(oddVertex[i], oddVertex[j], mnDist[i][j]);
-
-}
 
 int main (){
     while(scanf("%d", &n) != EOF && n){
         scanf("%d", &m);
         vector<vector<pair<int, ll>>> adj(n);
-        vector<vector<ll>> minDist(n, vector<ll>(n, INF));
+        mnDist.clear();
+        mnDist.resize(n);
+        for(int a=0;a<n;a++){
+            mnDist[a].resize(n);
+            for(int b=0;b<n;b++){
+                mnDist[a][b] = INF;
+            }
+        }
+        ll sum = 0;
+        for(int a=0;a<n;a++)
+            mnDist[a][a] = 0;
         for(int a=0;a<m;a++){
             int i, j;
             ll c;
@@ -74,9 +101,11 @@ int main (){
             adj[j].pb({i, c});
             mnDist[i][j] = min(mnDist[i][j], c);
             mnDist[j][i] = min(mnDist[j][i], c);
+            sum += c;
         }
-        floyd_warshall(mnDist);
-        vector<pair<int, int>> M = perfect_min_cost_match(adj, mnDist);  
+        floyd_warshall();
+        //vector<pair<int, int>> M = perfect_min_cost_match(adj, mnDist);  
+        printf("%lld\n", sum + cost_of_duplication(adj));
     }
 }
 
