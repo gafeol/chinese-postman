6c90fd6df69dfb9da4564074feb75f913a157644
gafeol <gabfeol@gmail.com>
2020-02-13 19:29:03 -0800

    Adds eulercycle code to pdf

--- euler_cycle.cpp
+++ euler_cycle.cpp
@@ -2,16 +2,11 @@ stack<int> path;
 vector<int> adj[N];
 int cur_ed[N];
 
-void add_edge(int u, int v){
-	adj[a].push_back(b);	
-	adj[b].push_back(a);
-}
-
 void euler_dfs(int u){
 	while(cur_ed[u] < adj[u].size()){
         int nxt = adj[u][cur_ed[u]];
         euler_dfs(nxt);
         cur_ed[u]++;
 	}
-	final_path.push(u);
+	path.push(u);
 }
