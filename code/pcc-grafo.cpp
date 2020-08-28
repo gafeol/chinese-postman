4b4f7c2564bfa102cf32b128d91525f377beb317
gafeol <gabfeol@gmail.com>
2020-08-28 17:24:45 -0300

    Adiciona teste para MCM

---
+++ code/pcc-grafo.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "min-cost-matching/MCM.hpp"
+
+struct CPP {
+    
+};
---
+++ test/min-cost-matching/MCM.cpp
@@ -0,0 +1,30 @@
+#include "gtest/gtest.h"
+#include "../../code/min-cost-matching/MCM.cpp"
+#include <bits/stdc++.h>
+using namespace std;
+
+void ord(vector<pair<int, int>> &v){
+    for(pair<int, int> &p: v){
+        if(p.first > p.second)
+            swap(p.first, p.second);
+    }
+    sort(v.begin(), v.end());
+}
+
+TEST(MCPM, SimpleMatching){
+    vector<tuple<int, int, double>> arestas = {
+        {0, 1, 1.},
+        {0, 2, 2.},
+        {1, 2, 0.},
+        {2, 3, 7.},
+        {1, 3, 3.}
+    };
+    pair<double, vector<pair<int, int>>> ans = MinimumCostPerfectMatching(4, arestas);
+    double cost = ans.first;
+    EXPECT_EQ(cost, 5);
+
+    vector<pair<int, int>> matching = ans.second;
+    vector<pair<int, int>> optMatching = {{2, 0}, {1, 3}};
+    ord(matching); ord(optMatching);
+    EXPECT_EQ(matching, optMatching);
+}
