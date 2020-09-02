d528f9e946b45b92e30c4c19b7aeefae57dbbc21
gafeol <gabfeol@gmail.com>
2020-09-01 22:05:45 -0300

    Adiciona maketuple no pcc tambem

--- test/pcc-grafo.cpp
+++ test/pcc-grafo.cpp
@@ -6,7 +6,7 @@ using namespace std;
 #include "../code/pcc-grafo.cpp"
 
 TEST(PCCGrafo, Simples){
-    Grafo G(2, { {0, 1, 2.}});
+    Grafo G(2, { make_tuple(0, 1, 2.)});
     PCC pcc;
     pair<double, vector<int>> ans = pcc.solve(G);
     EXPECT_EQ(ans.first, 4);
