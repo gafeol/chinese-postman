79d26baebd1168389c84f1136f59eb44be19a124
gafeol <gabfeol@gmail.com>
2020-09-09 15:17:22 -0300

    Adiciona floyd pra digrafos + teste

--- test/floyd-warshall.cpp
+++ test/floyd-warshall.cpp
@@ -53,3 +53,46 @@ TEST(FloydWarshall, Disconexo){
     EXPECT_EQ(d[2][1], INF);
     EXPECT_EQ(d[2][2], 0);
 }
+
+/*
+0 1 3.
+1 2 3.
+2 3 6.
+3 0 2.
+3 1 1.
+0 3 1.
+*/
+
+TEST(FloydWarshall, Digrafo){
+    int n = 4;
+    Digrafo G(n, { make_tuple(0, 1, 3.),
+                    make_tuple(1, 2, 3.),
+                    make_tuple(2, 3, 6),
+                    make_tuple(3, 0, 2),
+                    make_tuple(3, 1, 1),
+                    make_tuple(0, 3, 1)});
+    
+    vector<vector<double>> d = floyd_warshall(G);
+    EXPECT_EQ(d.size(), n);
+    for(int i=0;i<(int)d.size();i++)
+        EXPECT_EQ(d[i].size(), n);
+    EXPECT_EQ(d[0][0], 0);
+    EXPECT_EQ(d[0][1], 2);
+    EXPECT_EQ(d[0][2], 5);
+    EXPECT_EQ(d[0][3], 1);
+
+    EXPECT_EQ(d[1][0], 11);
+    EXPECT_EQ(d[1][1], 0);
+    EXPECT_EQ(d[1][2], 3);
+    EXPECT_EQ(d[1][3], 9);
+
+    EXPECT_EQ(d[2][0], 8);
+    EXPECT_EQ(d[2][1], 7);
+    EXPECT_EQ(d[2][2], 0);
+    EXPECT_EQ(d[2][3], 6);
+
+    EXPECT_EQ(d[3][0], 2);
+    EXPECT_EQ(d[3][1], 1);
+    EXPECT_EQ(d[3][2], 4);
+    EXPECT_EQ(d[3][3], 0);
+}
