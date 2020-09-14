c4bbdbb708cdf431a4bdad16b224bde3d7ad1705
gafeol <gabfeol@gmail.com>
2020-09-14 11:03:51 -0300

    Adiciona testes mais complexos Transporte

--- test/problema-transporte.cpp
+++ test/problema-transporte.cpp
@@ -21,4 +21,34 @@ TEST(ProblemaTransporte, RedeSimples){
     EXPECT_EQ(pt.flow, 2);
 }
 
+TEST(ProblemaTransporte, RedeSimples2){
+    ProblemaTransporte pt(2, {0}, {2}, 
+                             {1}, {1},
+                             { { 0., 0.4},
+                                { 2., 0. }});
+    
+    vector<tuple<int, int, int>> ans = pt.solve();
+    EXPECT_EQ(ans.size(), 1);
+    int u, v, f;
+    tie(u, v, f) = ans[0];
+    EXPECT_EQ(u, 0);
+    EXPECT_EQ(v, 1);
+    EXPECT_EQ(f, 1);
 
+    EXPECT_DOUBLE_EQ(pt.cost, 0.4);
+    EXPECT_EQ(pt.flow, 1);
+}
+
+TEST(ProblemaTransporte, Rede){
+    ProblemaTransporte pt(4, {0, 2}, {2, 3}, 
+                             {1, 3}, {4, 1},
+                             { { 0, 2, INT_MAX, 1},
+                               { INT_MAX, INT_MAX, INT_MAX, INT_MAX},
+                               { INT_MAX, 3, INT_MAX, 7 },
+                               { INT_MAX, INT_MAX, INT_MAX, INT_MAX }
+                             });
+    vector<tuple<int, int, int>> ans = pt.solve();
+    sort(ans.begin(), ans.end());
+    vector<tuple<int, int, int>> exp = {{0, 1, 1}, {0, 3, 1}, {2, 1, 3}};
+    EXPECT_EQ(ans, exp);
+}
