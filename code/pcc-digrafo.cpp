6ab3950525806cecc3312cad28982f97391d682b
gafeol <gabfeol@gmail.com>
2020-09-15 11:40:51 -0300

    Avança no codigo para pcc em digrafo,

--- code/pcc-digrafo.cpp
+++ code/pcc-digrafo.cpp
@@ -4,22 +4,22 @@ using namespace std;
 #include "min-cost-matching/MCM.cpp"
 #include "digrafo.hpp"
 #include "floyd-warshall.cpp"
-#include "euler-grafo.cpp"
+#include "euler-digrafo.cpp"
 #include "problema-transporte.cpp"
 
 struct PCC {
     private:
     Digrafo G;
 
-    vector<pair<int, int>> expande(int ini, int fim, vector<vector<double>> &mnDist){
+    vector<Aresta> expande(int ini, int fim, vector<vector<double>> &mnDist){
         if(ini == fim)
             return {};
         vector<vector<Aresta>> &adj = G.adj;
-        vector<pair<int, int>> ans;
+        vector<Aresta> ans;
         for(Aresta ar: adj[ini]){
             if(ar.cus + mnDist[ar.prox][fim] == mnDist[ini][fim]){
-                ans.push_back({ini, ar.prox});
-                vector<pair<int, int>> aux = expande(ar.prox, fim, mnDist);
+                ans.push_back(ar);
+                vector<Aresta> aux = expande(ar.prox, fim, mnDist);
                 ans.insert(ans.end(), aux.begin(), aux.end());
                 return ans;
             }
@@ -43,21 +43,38 @@ struct PCC {
             }
             else if(demanda[u] > 0){
                 S.push_back(u);
-                dS.push_back(demanda[u])
+                dS.push_back(demanda[u]);
             }
         }
 
         ProblemaTransporte pt(G.n, F, dF, S, dS, mnDist); // dist ou mnDist?
         vector<tuple<int, int, int>> f = pt.solve();
 
+        vector<tuple<int, int, double>> listaArcos = G.listaArcos();
+        vector<int> realId;
+        for(int i=0;i<(int)listaArcos.size();i++){
+            realId.push_back(i);
+        }
+
         for(tuple<int, int, int> tp: f){
             int u, v, flow;
             tie(u, v, flow) = tp;
             while(flow--){
-                expande(u, v, mnDist);
+                vector<Aresta> arcos = expande(u, v, mnDist);
+                for(Aresta arco: arcos){
+                    realId.push_back(arco.id);
+                    listaArcos.emplace_back(u, arco.prox, arco.cus);
+                    u = arco.prox;
+                }
+
             }
         }
-    }
 
-    
+        Euler e = Euler(G.n, listaArcos);
+        vector<int> trilha = e.trilha_euleriana_id();
+        for(int i=0;i<(int)trilha.size();i++){
+            trilha[i] = realId[trilha[i]];
+        }
+        return triliha;
+    }
 };
