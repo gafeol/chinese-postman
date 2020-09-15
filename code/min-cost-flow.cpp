4f69764c1125840f35214786698b34b0c8026ebe
gafeol <gabfeol@gmail.com>
2020-09-15 11:13:28 -0300

    Atualiza mincostflow

--- code/min-cost-flow.cpp
+++ code/min-cost-flow.cpp
@@ -6,24 +6,24 @@ typedef double num; // custos
 
 struct MinCostFlow {
     int N, M, en;
-    vector<int> es, to, nx, pai;
+    vector<int> es, to, nx, pai, seen, qu;
     vector<val> fl, cp;
     vector<val> flowOnEdge;
     vector<int> edgeId;
     vector<num> cs, d;
-    const num inf = 1e8, eps = 1e-8;
-    const val infv = INT_MAX;
-    vector<int> seen, qu;
+    const num eps = 1e-8;
     int tempo;
     num tot;
 
     MinCostFlow() {}
 
     MinCostFlow(int n): N(n), M(0) {
-        en = 0;
+        en = 2;
         tempo = 0;
+        fl.clear(); cp.clear(); nx.clear(); cs.clear(); to.clear();
+        fl.resize(2); cp.resize(2); nx.resize(2); cs.resize(2); to.resize(2);
         es.clear();
-        es.resize(N, -1);
+        es.resize(N, 0);
         pai.resize(N);
         d.resize(N);
         seen.resize(N);
@@ -31,10 +31,12 @@ struct MinCostFlow {
     }
 
     MinCostFlow(int n, int m): N(n), M(m) {
-        en = 0;
+        en = 2;
         tempo = 0;
+        fl.clear(); cp.clear(); nx.clear(); cs.clear(); to.clear();
+        fl.resize(2); cp.resize(2); nx.resize(2); cs.resize(2); to.resize(2);
         es.clear();
-        es.resize(N, -1);
+        es.resize(N, 0);
         pai.resize(N);
         d.resize(N);
         seen.resize(N);
@@ -45,42 +47,35 @@ struct MinCostFlow {
         edgeId.resize(M);
     }
 
-    val spfa(int s, int t) {
-        tempo++;
-        int a = 0, b = 0;
-        for(int i = 0; i < N; i++) d[i] = inf;
-        d[s] = 0;
-        qu[b++] = s;
-        seen[s] = tempo;
-        while(a != b) {
-            int u = qu[a++]; if(a == N) a = 0;
-            seen[u] = 0;
-            for(int e = es[u]; e != -1; e = nx[e])
-                if(cp[e] - fl[e] > val(0) && d[u] + cs[e] < d[to[e]] - eps) {
-                    d[to[e]] = d[u] + cs[e]; pai[to[e]] = e ^ 1;
-                    if(seen[to[e]] < tempo) { seen[to[e]] = tempo; qu[b++] = to[e]; if(b == N) b = 0; }
-                }
-        }
-        if(d[t] == inf) return false;
-        val mx = infv;
-        for(int u = t; u != s; u = to[pai[u]])
-            mx = min(mx, cp[pai[u] ^ 1] - fl[pai[u] ^ 1]);
-        tot += d[t] * val(mx);
-        for(int u = t; u != s; u = to[pai[u]])
-            fl[pai[u]] -= mx, fl[pai[u] ^ 1] += mx;
-        return mx;
-    }
+	val spfa(int s, int t) {
+		tempo++; int a = 0, b = 0;
+		for(int i = 0; i < N; i++) d[i] = numeric_limits<num>::max();
+		d[s] = 0; qu[b++] = s; seen[s] = tempo;
+		while(a != b) {
+			int u = qu[a++]; if(a == N) a = 0; seen[u] = 0;
+			for(int e = es[u]; e; e = nx[e]) if(cp[e] - fl[e] > val(0) && d[u] + cs[e] < d[to[e]] - eps) {
+				d[to[e]] = d[u] + cs[e]; pai[to[e]] = e ^ 1;
+				if(seen[to[e]] < tempo) { seen[to[e]] = tempo; qu[b++] = to[e]; if(b == N) b = 0; }
+			}
+		}
+		if(d[t] == numeric_limits<num>::max()) return false;
+		val mx = numeric_limits<val>::max();
+		for(int u = t; u != s; u = to[pai[u]])
+			mx = min(mx, cp[pai[u] ^ 1] - fl[pai[u] ^ 1]);
+		tot += d[t] * val(mx);
+		for(int u = t; u != s; u = to[pai[u]])
+			fl[pai[u]] -= mx, fl[pai[u] ^ 1] += mx;
+		return mx;
+	}
     
     val flow;
     num solve(int s, int t) {
         tot = 0; flow = 0;
         while(val a = spfa(s, t)) flow += a;
-        /*
-        printf("FLOW de %d a %d deu %d\n", s, t, flow);
+        /*printf("FLOW de %d a %d deu %d\n", s, t, flow);
         for(int a=0;a<en;a++){
             printf("    flow on en %d (to %d cp %d fl %d cs %.3f\n", a, to[a], cp[a], fl[a], cs[a]);
-        }
-        */
+        }*/
         for(int i=0;i<M;i++)
             flowOnEdge[i] = fl[edgeId[i]];
         return tot;
