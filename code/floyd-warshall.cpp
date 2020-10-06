1a86808e7666cb8738f064b520abe9c4d8aec3b5
gafeol <gabfeol@gmail.com>
2020-10-06 09:58:40 -0300

    adiciona floyd para grafo misto

--- code/floyd-warshall.cpp
+++ code/floyd-warshall.cpp
@@ -3,6 +3,7 @@
 
 #include "grafo.hpp"
 #include "digrafo.hpp"
+#include "grafo-misto.hpp"
 #include "bits/stdc++.h"
 using namespace std;
 
@@ -40,6 +41,31 @@ vector<vector<double>> floyd_warshall(Digrafo G){
             int nxt = ar.prox;
             double cus = ar.cus;
             dist[i][nxt] = min(dist[i][nxt], cus);
+        }
+    }
+    for(int k=0;k<n;k++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(dist[i][j] > dist[i][k] + dist[k][j]){
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+    return dist;
+}
+
+vector<vector<double>> floyd_warshall(Misto G){
+    int n = G.n;
+    vector<vector<double>> dist(n, vector<double>(n, INF));
+    for(int i=0;i<n;i++){
+        dist[i][i] = 0;
+        for(Aresta ar: G.adj[i]){
+            int nxt = ar.prox;
+            double cus = ar.cus;
+            dist[i][nxt] = min(dist[i][nxt], cus);
+            if(G.aresta(ar.id))
+                dist[nxt][i] = min(dist[nxt][i], cus);
         }
     }
     for(int k=0;k<n;k++){
