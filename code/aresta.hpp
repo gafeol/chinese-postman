093493a369cf88b9f2523540ea4dae759a45b07c
gafeol <gabfeol@gmail.com>
2020-10-15 21:31:49 -0300

    Adiciona operador de igualdade para arestas

--- code/aresta.hpp
+++ code/aresta.hpp
@@ -11,6 +11,14 @@ struct Aresta{
     Aresta(int prox, int id) : prox(prox), id(id) {}
     Aresta(int prox, double cus) : prox(prox), cus(cus) {}
     Aresta(int prox, int id, double cus) : prox(prox), id(id), cus(cus) {}
+
+    bool operator ==(const Aresta &o) const {
+        return (prox == o.prox && id == o.id && cus == o.cus);
+    }
+
+    void print(){
+        printf("(prox %d id %d cus %.3f)\n", prox, id,cus);
+    }
 };
 
 #endif
