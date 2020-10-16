2b894b5fc08199c24047f513c763b20d01007547
gafeol <gabfeol@gmail.com>
2020-10-15 21:32:08 -0300

    Adiciona teste inicial pcc misto

--- test/pcc-misto.cpp
+++ test/pcc-misto.cpp
@@ -3,11 +3,30 @@
 using namespace std;
 
 #include "../code/euler-misto.cpp"
+#include "../code/pcc-misto.cpp"
 
 
+TEST(PCCMisto, GrauTotalPar){
+    Misto G(2, {{1, 0}}, {{0, 1}});
+    EXPECT_EQ(G.grauEntrada(0), 0);
+    EXPECT_EQ(G.grauSaida(0), 1);
+    EXPECT_EQ(G.grau(0), 1);
+    PCC pcc;
+    Misto GPar = pcc.grau_total_par(G);
+    for(int u=0;u<G.n;u++){
+        EXPECT_EQ(G.adj[u], GPar.adj[u]);
+    }
+}
+
+/*
 TEST(PCCMisto, MixedTwoNodes){
-    Euler d(2, 1, {{1, 0}, {0, 1}});
-    EXPECT_FALSE(d.euleriano());
+    Misto G(2, {{1, 0}}, {{0, 1}});
+    PCC pcc;
+    auto sol = pcc.solveById(G);
+    for(int u: sol){
+        printf("%d ", u);
+    }
+    puts("");
 }
 
 TEST(PCCMisto, MixedTwoNodesMultipleArcs){
@@ -19,3 +38,4 @@ TEST(PCCMisto, MixedThreeNodes){
     Euler d(3, {{2, 0}}, {{0, 1}, {1, 2}});
     EXPECT_FALSE(d.euleriano());
 }
+*/
