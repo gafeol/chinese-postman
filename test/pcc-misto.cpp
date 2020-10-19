bc826aae7d6e68f0d0e694a0cd05bb58291bd1e4
gafeol <gabfeol@gmail.com>
2020-10-19 00:21:54 -0300

    Adiciona testes grau par e solve

--- test/pcc-misto.cpp
+++ test/pcc-misto.cpp
@@ -6,7 +6,7 @@ using namespace std;
 #include "../code/pcc-misto.cpp"
 
 PCC pcc;
-void testGrauPar(Misto G) { 
+void testGrauTotalPar(Misto G) { 
     for(int u=0;u<G.n;u++){
         EXPECT_EQ(G.grauTotal(u)%2, 0);
     }
@@ -15,6 +15,13 @@ void testGrauPar(Misto G) {
 void testGrauDir(Misto G){
     for(int u=0;u<G.n;u++){
         EXPECT_EQ(G.grauEntrada(u), G.grauSaida(u));
+    }
+}
+
+void testGrauPar(Misto G){
+    for(int u=0;u<G.n;u++){
+        EXPECT_EQ(G.grauEntrada(u), G.grauSaida(u));
+        EXPECT_EQ(G.grau(u)%2, 0);
     }
 }
 
@@ -30,7 +37,7 @@ TEST(PCCMisto, GrauTotalPar){
     for(int u=0;u<G.n;u++){
         EXPECT_EQ(G.adj[u], GPar.adj[u]);
     }
-    testGrauPar(GPar);
+    testGrauTotalPar(GPar);
 }
 
 TEST(PCCMisto, GrauTotalPar2){
@@ -38,7 +45,7 @@ TEST(PCCMisto, GrauTotalPar2){
     for(int u=0;u<2;u++)
         EXPECT_NE(G.grauTotal(u)%2, 0);
     Misto GPar = pcc.grau_total_par(G);
-    testGrauPar(GPar);
+    testGrauTotalPar(GPar);
 }
 
 TEST(PCCMisto, GrauDirIgual){
@@ -46,9 +53,12 @@ TEST(PCCMisto, GrauDirIgual){
     auto [M, U, MAdd] = pcc.iguala_grau_dir(G);
     Misto GDir(G.n, M, U);
     testGrauDir(GDir);
+    // Orienta aresta 1 - 0 para 1 -> 0
     EXPECT_EQ(MAdd.size(), 1);
+    EXPECT_EQ(GDir.grau(0), 0);
     EXPECT_EQ(GDir.grauEntrada(0), 1);
     EXPECT_EQ(GDir.grauSaida(0), 1);
+    EXPECT_EQ(GDir.grau(1), 0);
     EXPECT_EQ(GDir.grauEntrada(1), 1);
     EXPECT_EQ(GDir.grauSaida(1), 1);
 }
@@ -82,7 +92,49 @@ TEST(PCCMisto, GrauDirIgual3){
     testGrauDir(GDir);
 }
 
+TEST(PCCMisto, GrauPar){
+    Misto G(2, {{1, 0}}, {{0, 1}});
+    G = pcc.grau_total_par(G);
+    auto [M, U, MAdd] = pcc.iguala_grau_dir(G);
+    tie(M, U) = pcc.grau_par(G.n, M, U, MAdd);
+    EXPECT_EQ(M.size(), 1);
+    EXPECT_EQ(M[0], make_tuple(1, 0, 1., 0));
+    EXPECT_EQ(U.size(), 0);
+    for (int u = 0; u < G.n; u++) {
+        for (Aresta ar : G.adj[u]) {
+            if (G.arco(ar.id)) {
+                M.emplace_back(u, ar.prox, ar.cus, ar.id);
+            }
+        }
+    }
+    testGrauPar(Misto(G.n, M, U));
+}
 
+TEST(PCCMisto, GrauPar2){
+    Misto G(2, {{0, 1}}, {});
+    G = pcc.grau_total_par(G);
+    auto [M, U, MAdd] = pcc.iguala_grau_dir(G);
+    tie(M, U) = pcc.grau_par(G.n, M, U, MAdd);
+    for (int u = 0; u < G.n; u++) {
+        for (Aresta ar : G.adj[u]) {
+            if (G.arco(ar.id)) {
+                M.emplace_back(u, ar.prox, ar.cus, ar.id);
+            }
+        }
+    }
+    EXPECT_EQ(M.size(), 0);
+    EXPECT_EQ(U.size(), 2);
+    vector<tiidi> expU = {make_tuple(1, 0, 1., 0), make_tuple(1, 0, 1., 1)};
+    EXPECT_EQ(U, expU);
+    testGrauPar(Misto(G.n, M, U));
+}
+
+TEST(PCCMisto, Solve){
+    Misto G(2, {{0, 1}}, {});
+    auto trilha = pcc.solveById(G);
+    vector<int> expTrilha = {0, 0};
+    EXPECT_EQ(trilha, expTrilha);
+}
 /*
 TEST(PCCMisto, MixedTwoNodes){
     Misto G(2, {{1, 0}}, {{0, 1}});
