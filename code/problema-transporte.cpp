221b26db193a81fb84d42983e6845b2cea3251ed
gafeol <gabfeol@gmail.com>
2020-12-17 21:53:29 -0300

    Adiciona docstring problema transporte

--- code/problema-transporte.cpp
+++ code/problema-transporte.cpp
@@ -15,6 +15,7 @@ struct ProblemaTransporte {
 
     vector<pair<int, int>> arestas;
 
+    /// Recebe um grafo F,S-bipartido, onde os vértices u de F devem escoar dF[u] unidades e os vértices w de S tem demanda de dS[w] unidades.
     ProblemaTransporte(int N, vector<int> F, vector<int> dF, vector<int> S, vector<int> dS, vector<vector<double>> mnDist)
         : mcf(MinCostFlow(N+2, (int)F.size()*S.size())), s(N), t(N+1) {
 
